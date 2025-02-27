cc44f9c92fc915c568e9a761434c9f7547ea600d
l <a@b.com>
2025-02-27 23:45:39 +0800

    feat: 更新demo工程，从support库到androidx版本，升级gradle版本、minSdk、compileSdk和targetSdk，使其能够运行

--- app/src/main/java/com/naman14/tandroidlame/Mp3AudioRecordActivity.java
+++ app/src/main/java/com/naman14/tandroidlame/Mp3AudioRecordActivity.java
@@ -14,17 +14,21 @@
 
 package com.naman14.tandroidlame;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.naman14.androidlame.AndroidLame;
 import com.naman14.androidlame.LameBuilder;
@@ -95,6 +99,16 @@ public class Mp3AudioRecordActivity extends AppCompatActivity {
                 AudioFormat.ENCODING_PCM_16BIT);
 
         addLog("Initialising audio recorder..");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         audioRecord = new AudioRecord(
                 MediaRecorder.AudioSource.MIC, inSamplerate,
                 AudioFormat.CHANNEL_IN_MONO,
